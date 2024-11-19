0ba02116502179fa18d1b103b03982aa49501758
ZCShou <72115@163.com>
2024-11-19 17:49:24 +0800

    更新主界面

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,43 +1,98 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import Link from "@docusaurus/Link";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import "./index.css";
 
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+function HomepageBanner() {
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+    <div className="secos-banner">
+      <div className="secos-banner-container">
+        <div className="left">
+
+          <div className="secos-banner-title" >
+          SecOS
+          </div>
+
+          <div className="secos-banner-description" style={{ position: "relative", zIndex: 2 }}>
+            专注于 AIoT 领域的安全操作系统内核
+          </div>
+
+          <div style={{ position: "relative", zIndex: 2 }}>
+            <ul className="secos-banner-spec">
+              <li>MIT 宽松开源协议</li>
+              <li>模块化设计</li>
+              <li>Rust 语言</li>
+              <li>麻雀虽小五脏俱全</li>
+            </ul>
+          </div>
+          <div className="secos-support-platform">多架构支持：</div>
+          <div className="secos-support-icons">
+            <span>
+            <img src="img/arm.svg"></img>
+            </span>
+            <span>
+            <img src="img/x86.svg"></img>
+            </span>
+          </div>
+
+          <div className="secos-button">
+              <Link className="secos-explore-button" to={useBaseUrl("docs/intro")}>
+                入门指南
+              </Link>
+
+                <Link className="secos-download-button" to={useBaseUrl("docs/intro")}>
+                  下载
+                  <span className="secos-version">v4.9.1.7</span>
+                </Link>
+            </div>
+        </div>
+        
+        <div className="right">
+          <img src="img/secos.arch.svg"></img>
         </div>
       </div>
-    </header>
+    </div>
   );
 }
 
-export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+function HomepageModule() {
+  return (
+    <div className="secos-module">
+      <div className="secos-module-container">
+        <div className="left">
+        <img src="img/secos.module.png"></img>
+        </div>
+        <div className="right">
+        <div style={{ position: "relative", zIndex: 2 }}>
+            <ul className="secos-banner-spec">
+              <li>模块 1</li>
+              <li>模块 2</li>
+              <li>模块 3</li>
+              <li>模块 4</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Home() {
+  const context = useDocusaurusContext();
+  const { siteConfig = {} } = context;
+
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      title={"专注于 AIoT 领域的安全操作系统内核"}
+      description="专注于 AIoT 领域的安全操作系统内核"
+    >
+      <HomepageBanner />
+
+      <HomepageModule />
+
     </Layout>
   );
 }
+
+export default Home;
--- src/pages/markdown-page.md
+++
@@ -1,7 +0,0 @@
----
-title: Markdown 页面示例
----
-
-# Markdown page example
-
-You don't need React to write simple standalone pages.
